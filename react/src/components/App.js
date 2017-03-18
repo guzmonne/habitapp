af7a3077970d4f89e206bd93d3083e05938df92e
Guzman Monne <guzmonne@hotmail.com>
2017-03-18 20:31:53 -0300

    added a precopy task

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from '../_images/logo.svg';
 import '../_styles/App.css';
+import Header from './Header.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
       </div>
     );
   }
---
+++ react/src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Header = () => (
+  <h1>Habitapp!</h1>
+)
+
+export default Header
